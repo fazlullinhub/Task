c354f5c0ae6414afc6604e960a88e0ccb17487d5
fazlullinhub <164046064+fazlullinhub@users.noreply.github.com>
2024-06-20 11:12:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp22
+{
+    
+    public class MyList
+    {
+        public List<int> list = new List<int>();
+        public void Add()
+        {
+            List<int> tmp = new List<int>(list); 
+            tmp.Add(0); 
+            list = tmp; 
+        }
+
+        public void Pop()
+        {
+            if (list.Count > 0) 
+            {
+                list.RemoveAt(list.Count - 1); 
+            }
+            else
+            {
+                Console.WriteLine("List is empty"); 
+            }
+        }
+
+        public void Show()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item); 
+            }
+        }
+    }
+}
